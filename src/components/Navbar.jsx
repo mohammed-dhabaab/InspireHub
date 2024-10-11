1f5284dc3af9cf9ad79a91329a3de997c10d3ddf
comsaziz <comsaziz@gmail.com>
2024-10-11 23:03:14 +0300

    Add Link to the Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,62 +1,58 @@
 import React from "react";
-import { useLocation } from "react-router-dom"; 
+import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
 import styles from "../styles";
 import Logo from "../assets/logo/logo-light.png";
 import { IoLogOut } from "react-icons/io5";
 
 function Navbar() {
-  const location = useLocation(); 
+  const location = useLocation();
 
   return (
     <main className={`py-4 bg-primary`}>
       <div
-        className={`${styles.wrapper} flex justify-between items-center text-white cursor-pointer`}
+        className={`${styles.wrapper} flex justify-between items-center text-white `}
       >
-        
         <div className="flex-1">
-          <img className="w-32" src={Logo} alt="Logo" />
+          <img className="w-32 cursor-pointer" src={Logo} alt="Logo" />
         </div>
 
-     
         <div className="flex items-center space-x-4 ml-auto ">
           {location.pathname === "/admin" && (
             <>
               <Link to={"/studentideas"}>
-
-              <p className="hover:text-[#53e3d7]">Students</p>
-
+                <p className="hover:text-[#53e3d7] cursor-pointer">Students</p>
               </Link>
               <Link to={"/"}>
-
-              <IoLogOut className="hover:text-[#53e3d7]" size={23} />
-
+                <IoLogOut
+                  className="hover:text-[#53e3d7] cursor-pointer"
+                  size={23}
+                />
               </Link>
             </>
           )}
 
           {location.pathname === "/studentideas" && (
             <>
-
-            <Link to={"/Home"}>
-
-            <p className="hover:text-[#53e3d7]">Home</p>
-
-            </Link>
-              
-              <Link to={"/"}>
-
-              <IoLogOut className="hover:text-[#53e3d7]" size={23} />
-
+              <Link to={"/home"}>
+                <p className="hover:text-[#53e3d7] cursor-pointer">Home</p>
               </Link>
-
-
+              <Link to={"/"}>
+                <IoLogOut
+                  className="hover:text-[#53e3d7] cursor-pointer"
+                  size={23}
+                />
+              </Link>
             </>
           )}
-
           {location.pathname === "/home" && (
             <>
-              
+              <Link to="/">
+                <IoLogOut
+                  className="hover:text-[#53e3d7] cursor-pointer"
+                  size={23}
+                />
+              </Link>
             </>
           )}
         </div>
