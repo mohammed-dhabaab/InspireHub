f03089710ab31f365631045c3f31b9da5dca8cb8
comsaziz <comsaziz@gmail.com>
2024-10-13 01:12:15 +0300

    solve conflicts

--- src/pages/StudentIdeas/StudentIdeas.jsx
+++ src/pages/StudentIdeas/StudentIdeas.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import styles from '../../styles'
+import { useEffect, useState } from 'react'
 
 function StudentIdeas() {
   return (
