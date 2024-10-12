9b7a9737328bb4460fd407827c75aba4cdea05be
mohammed-dhabaab <mohammed.dhabaab@gmail.com>
2024-10-12 10:49:43 +0300

    Started my branch

--- src/Router.jsx
+++ src/Router.jsx
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
         )
     },
     {
-        path: '/studentideas',
+        path: '/studentideas/:id',
         errorElement: <ErrorPage />,
         element: (
             <>
--- src/pages/StudentIdeas/StudentIdeas.jsx
+++ src/pages/StudentIdeas/StudentIdeas.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import styles from '../../styles'
+import { useEffect, useState } from 'react'
 
 function StudentIdeas() {
+    const { id } = useParams()
+    const [student, setStudent] = useState({})
+    const [studentIdeas, setStudentIdeas] = useState([])
+    const [filteredStudentIdeas, setFilteredStudentIdeas] = useState([])
+
+    const getStudentData = async () => {
+        try {
+            const response = await fetch(`http://localhost:5000/students/${id}`)
+            const data = await response.json()
+            setStudent(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+
+    })
+
+
     return (
-        <div>StudentIdeas</div>
+        <main className={`${styles.outerWrapper}`}>
+            <div className={`${styles.wrapper}`}>
+                <h1>Student Name</h1>
+            </div>
+        </main>
     )
 }
 
