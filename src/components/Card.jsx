702e7573949c1dd6742d0df19185c28d00d8b96b
Rana Almalki <ranaalmalki12@gmail.com>
2024-10-13 17:47:14 +0300

    edit the card color by status & get all ideas the acceptable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,35 @@
 import styles from "../styles"
-function Card({username , imgprofile ,title, description ,onEdit ,buttonText}) {
+function Card({username , imgprofile ,title, description ,onEdit ,buttonText ,status}) {
+    const statusStyles = {
+        Acceptable: {
+        //   background: "bg-green-50",
+          boxShadow: "[box-shadow:12px_12px_0px_0px_rgba(144,211,214,0.5)]",
+          hoverBoxShadow: "hover:[box-shadow:4px_4px_0px_0px_rgba(144,211,214,0.5)]"
+        },
+        Rejected: {
+        //   background: "bg-red-50",
+          boxShadow: "[box-shadow:12px_12px_0px_0px_rgba(255,0,0,0.5)]",
+          hoverBoxShadow: "hover:[box-shadow:4px_4px_0px_0px_rgba(255,0,0,0.5)]"
+        },
+        Edited: {
+        //   background: "bg-yellow-50",
+          boxShadow: "[box-shadow:12px_12px_0px_0px_rgba(255,255,0,0.5)]",
+          hoverBoxShadow: "hover:[box-shadow:4px_4px_0px_0px_rgba(255,255,0,0.5)]"
+        },
+        default: {
+        //   background: "bg-gray-50",
+          boxShadow: "[box-shadow:12px_12px_0px_0px_rgba(0,0,0,0.5)]",
+          hoverBoxShadow: "hover:[box-shadow:4px_4px_0px_0px_rgba(0,0,0,0.5)]"
+        },
+      };
+      const {boxShadow , hoverBoxShadow}=statusStyles[status] || statusStyles.default
   return (
     <div className={`${styles.outerWrapper}`}>
 <div
-  className="relative rounded-lg -skew-x-6 -translate-y-2 -translate-y-6 hover:-translate-y-1 hover:-translate-x-0 hover:skew-x-0 duration-500
-   w-72 h-44 p-2 bg-neutral-50 card-compact hover:bg-base-200 transition-all duration-200 [box-shadow:12px_12px_0px_0px_rgba(144,211,214,0.5)] hover:[box-shadow:4px_4px_0px_0px_rgba(144,211,214,0.5)] "
+  className={`relative rounded-lg -skew-x-6 -translate-y-2 -translate-y-6 hover:-translate-y-1 hover:-translate-x-0 hover:skew-x-0 duration-500
+   w-72 h-44 p-2 bg-neutral-50 card-compact hover:bg-base-200 transition-all duration-200 ${boxShadow} ${hoverBoxShadow} `}
 >
-  <figure className="w-full h-full">
+  <figure className="w-full h-full ">
     <div
       alt="change to a img tag"
       className="bg-neutral-50 text-neutral-50 min-h-full rounded-lg border border-opacity-5"
@@ -18,7 +41,7 @@ function Card({username , imgprofile ,title, description ,onEdit ,buttonText}) {
     <img className="w-10 h-10 rounded-full" src={`${imgprofile}`} alt="Rounded avatar"/>
     <p className={`${styles.paragraph4}`}>{username}</p>
     </div>
-    <button onClick={onEdit} className=" text-black hover:text-green-600 hover: px-2 py-9 rounded ml-28 ">{buttonText}</button>
+    <button onClick={onEdit} className=" text-black hover:text-green-600 hover: px-2 py-9 rounded  ">{buttonText}</button>
     </figure>
 
     <span className="font-bold">{title}</span>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,114 +1,169 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import Card from "../../components/Card";
 import styles from "../../styles";
 import searchForTerm from "../../utils/searchForTerm";
 import { FaRegEdit } from "react-icons/fa";
-import ideasphoto from "../../assets/Home/ideaphoto.png"
-import ideaedit from "../../assets/Home/editidea.png"
+import ideasphoto from "../../assets/Home/ideaphoto.png";
+import ideaedit from "../../assets/Home/editidea.png";
 
 function Home() {
-  const [ideas, setIdeas] = useState([
-
-    {
-        id: 1,
-        username: "Rana",
-        imgprofile: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
-        title: "فكرة 1",
-        description: "وصف الفكرة الأولى.",
-      },
-      {
-        id: 2,
-        username: "Rana",
-        imgprofile: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
-        title: "فكرة 2",
-        description: "وصف الفكرة الثانية.",
-      },
-      {
-        id: 3,
-        username: "Rana",
-        imgprofile: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
-        title: "فكرة 3",
-        description: "وصف الفكرة الثالثة.",
-      },
-      {
-        id: 4,
-        username: "Rana",
-        imgprofile: "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
-        title: "فكرة 4",
-        description: "وصف الفكرة الرابعه.",
-      },
-
-  ]);
+  const [ideas, setIdeas] = useState([]);
   const [filteredArr, setFiltredArr] = useState([]);
   const [searchTerm, setSerachTerm] = useState("");
-  const [newIdea,setNewIdea]=useState({
-    title:"",description:"",username:"",imgprofile:""
-  })
-const [isPopupOpen, setIsPopupOpen]=useState(false)
-const [editIdea,setEditeIdea]=useState(null)
-const [activeTap , setActiveTap]=useState("myIdeas")
-  const addIdea =()=>{
-    if(newIdea.title && newIdea.description){
-        setIdeas([...ideas,{...newIdea, id:ideas.length +1}])
-        setFiltredArr([...filteredArr],{...newIdea,id:ideas.length+1})
-        setNewIdea({title:"",description:"",username:"Rana",imgprofile:""})
-        setIsPopupOpen(false);
+  const [userInfo, setUserInfo] = useState(null);
+  const [newIdea, setNewIdea] = useState({
+    title: "",
+    description: "",
+    name: "",
+    avatar: "",
+  });
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [editIdea, setEditeIdea] = useState(null);
+  const [activeTap, setActiveTap] = useState("myIdeas");
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userResponse = await axios.get(
+          "https://670941a3af1a3998baa0ec5c.mockapi.io/users"
+        );
+        console.log("User response:", userResponse.data);
+        if(userResponse.data.length >0){
+            const userData = userResponse.data[2]
+            console.log(userData);
+            
+            setUserInfo(userData);
+            setNewIdea((prev) => ({ ...prev, name: userData.name ,avatar:userData.avatar }));
+        }else{
+            console.log("no user data found");
+            
+        }
+       
+      } catch (error) {
+        console.log("Error fetching user data:", error);
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  useEffect(()=>{
+    const fetchIdeas = async ()=>{
+        if(!userInfo) {
+            console.log("User info is not available yet.");
+            return ;
+        }
+        try{
+            const response = await axios.get("https://670941a3af1a3998baa0ec5c.mockapi.io/ideas")
+            const allideass = response.data
+            const StudentId =userInfo.StudentId
+console.log(allideass);
+
+            const StudentIdeas = allideass.filter(idea =>
+                idea.StudentId === StudentId)
+            console.log("Filtered ideas:", StudentIdeas); 
+            
+            const ideasWithUsernames = StudentIdeas.map(idea =>{
+                // const user = userInfo;
+                // console.log(user);
+                
+                return{
+                    ...idea,
+                    username:userInfo.name,
+                    avatar:userInfo.avatar,
+                    status:idea.status|| 'default'
+                    
+                    
+                }
+            })
+            console.log(ideasWithUsernames);
+
+            setIdeas(ideasWithUsernames)
+        } catch(error){
+            console.error("Error fetching ideas:", error);
+        }
     }
+    fetchIdeas();
+  },[userInfo])
 
-   
-  }
-  const updateIdea =()=>{
-    if(editIdea.title && editIdea.description){
-        setIdeas(ideas.map(idea=>
-        idea.id === editIdea.id ? editIdea: idea
-        ))
-        setEditeIdea(null)
+  
+  const addIdea = async () => {
+    if (newIdea.title && newIdea.description) {
+        const userId = userInfo.id
+
+        const ideatoAdd ={...newIdea,userId}
+        try{
+            const response = await axios.post("https://670941a3af1a3998baa0ec5c.mockapi.io/ideas",ideatoAdd)
+            setIdeas([...ideas,response.data]);
+      setFiltredArr([...filteredArr ,response.data]);
+      setNewIdea({
+        title: "",
+        description: "",
+        name: newIdea.name,
+        avatar: newIdea.avatar,
+      });
+      setIsPopupOpen(false);
+        }catch (error) {
+            console.error("Error adding idea:", error);
+          }
+  
     }
-}
+  };
+  const updateIdea = async () => {
+    if (editIdea.title && editIdea.description) {
+        try{
+            const response = await axios.put(`https://670941a3af1a3998baa0ec5c.mockapi.io/ideas/${editIdea.id}`,editIdea)
+            setIdeas(
+                ideas.map((idea) => (idea.id === editIdea.id ? response.data : idea))
+              );
+              setEditeIdea(null);
+            }catch (error) {
+                console.error("Error updating idea:", error);
+              }
+        }
+     
+  };
+  const fitredIdeas = activeTap==="AllIdeas"
+  ?ideas.filter(idea => idea.status === "Acceptable")
+  :ideas.filter(idea =>idea.StudentId === userInfo.StudentId)
 
-// useEffect(()=>{
-//     const results=searchForTerm(searchTerm,ideas);
-//     setFiltredArr(results);
+  useEffect(()=>{
+      const results=searchForTerm(searchTerm,ideas);
+      setFiltredArr(results);
 
-// },[searchTerm ,ideas])
-
+  },[searchTerm ,ideas])
 
   return (
-    <main className={`${styles.outerWrapper} `}>
-      <div className={`${styles.wrapper} flex justify-between`}>
-        <h2 className={styles.heading2}>Welcome Student Rana</h2>
+    <main className={`${styles.outerWrapper}`}>
+      <div className={`${styles.wrapper} flex justify-center text-primary`}>
+        <h2 className={`${styles.heading2}`}>Welcome Student <span className="text-secondary">{userInfo?.name}</span></h2>
       </div>
-     
 
       <div className={`${styles.wrapper} flex justify-center`}>
         <div className="text-center">
           <h3 className={styles.heading3}>Ideas</h3>
           <div className="w-full">
             <div className="relative right-0">
-              <ul
-              className="flex list-none"
-           
-              >
+              <ul className="flex list-none">
                 <li className="mr-4">
-                 
-                  
-                    <button className={`py-2 px-4 ${activeTap === "myIdeas"?"font-bold bg-slate-50":""}`}
-                    onClick={()=>setActiveTap("myIdeas")}
-
-                    >my ideas
-                    
-                    </button>
-                
+                  <button
+                    className={`py-2 px-4 ${
+                      activeTap === "myIdeas" ? "font-bold bg-slate-50" : ""
+                    }`}
+                    onClick={() => setActiveTap("myIdeas")}
+                  >
+                    my ideas
+                  </button>
                 </li>
                 <li>
-                 
-                    <button className={`py-2 px-4 ${activeTap === "AllIdeas"?"font-bold bg-slate-50":""}`}
-                    onClick={()=>setActiveTap("AllIdeas")}
-                    
-                    
-                    >All ideas</button>
-
+                  <button
+                    className={`py-2 px-4 ${
+                      activeTap === "AllIdeas" ? "font-bold bg-slate-50" : ""
+                    }`}
+                    onClick={() => setActiveTap("AllIdeas")}
+                  >
+                    All ideas
+                  </button>
                 </li>
               </ul>
             </div>
@@ -120,127 +175,140 @@ const [activeTap , setActiveTap]=useState("myIdeas")
               className={styles.Search}
             />
           </div>
-          {/* {filteredArr.map((idea) => (
-            <div key={idea.id}>
-              <h2>{idea.title}</h2>
-              <p>{idea.des}</p>
-            </div>
-          ))} */}
-        </div>
-
-        </div>
-        <div className={`${styles.wrapper} flex justify-between`}>
-        {isPopupOpen && (
-                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-
- <div className="bg-white p-8 rounded shadow-lg relative ">
- <button
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
-              onClick={() => setIsPopupOpen(false)}
-            >
-                 &times;
-                 </button>
-    <div className="flex flex-wrap items-center ">
-    <div className="mt-4">
-    <h4 className={`${styles.heading4} bg-primary text-white p-2`}>Add new idea</h4>
-        <label>Title</label>
- <input
- type="text"
- placeholder="title idea"
- value={newIdea.title}
- onChange={(e)=>{setNewIdea({...newIdea,title:e.target.value})}}
- className={`${styles.input} mb-4 `}
-
- />
- <br/>
- <label>description</label>
-
- <textarea
- placeholder="descripation the idea"
- value={newIdea.description}
- onChange={(e)=>setNewIdea({...newIdea,description:e.target.value})}
- className={`${styles.input} mb-4`}
-
- />
- <button onClick={addIdea} className={`${styles.paragraph4} bg-secondary p-2 rounded-full mt-2 w-full`}>add new idea</button>
- </div>
- <div className="w-96 h-96">
- <img
-src={ideasphoto}
-className="w-full h-full flip- "
-/>
-</div>
-</div>
-</div>
-
-</div>
-        )}
        
+        </div>
       </div>
-    <div className={`${styles.wrapper}`}>
-    <button className="btn text-white bg-primary p-2 rounded-full" onClick={()=>setIsPopupOpen(true)}>New idea +</button>
-<div className="flex flex-wrap justify-around gap-1">
-        {ideas.map((idea)=>(
-            <div className="flex-shrink-0 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2" key={idea.id}>
+      <div className={`${styles.wrapper} flex justify-between`}>
+        {isPopupOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white p-8 rounded shadow-lg relative ">
+              <button
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
+                onClick={() => setIsPopupOpen(false)}
+              >
+                &times;
+              </button>
+              <div className="flex flex-wrap items-center ">
+                <div className="mt-4">
+                  <h4
+                    className={`${styles.heading4} bg-primary text-white p-2`}
+                  >
+                    Add new idea
+                  </h4>
+                  <label>Title</label>
+                  <input
+                    type="text"
+                    placeholder="title idea"
+                    value={newIdea.title}
+                    onChange={(e) => {
+                      setNewIdea({ ...newIdea, title: e.target.value });
+                    }}
+                    className={`${styles.input} mb-4 `}
+                  />
+                  <br />
+                  <label>description</label>
 
-                <Card
+                  <textarea
+                    placeholder="descripation the idea"
+                    value={newIdea.description}
+                    onChange={(e) =>
+                      setNewIdea({ ...newIdea, description: e.target.value })
+                    }
+                    className={`${styles.input} mb-4`}
+                  />
+                  <button
+                    onClick={addIdea}
+                    className={`${styles.paragraph4} bg-secondary p-2 rounded-full mt-2 w-full`}
+                  >
+                    add new idea
+                  </button>
+                </div>
+                <div className="w-96 h-96">
+                  <img src={ideasphoto} className="w-full h-full flip- " />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className={`${styles.wrapper}`}>
+        <button
+          className="btn text-white bg-primary p-2 rounded-full"
+          onClick={() => setIsPopupOpen(true)}
+        >
+          New idea +
+        </button>
+        <div className="flex flex-wrap justify-around gap-1">
+          {fitredIdeas.map((idea) => (
+            <div
+              className="flex-shrink-0 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2"
+              key={idea.id}
+            >
+              <Card
                 key={idea.id}
                 username={idea.username}
-                imgprofile={idea.imgprofile}
-                title={idea.title}
+                imgprofile={idea.avatar}
+                title={idea.name}
                 description={idea.description}
-                onEdit={()=>setEditeIdea(idea)}
-                buttonText={<FaRegEdit className="w-5 h-5"/>
-                }
-                />
-</div>
-
-        ))}
+                onEdit={() => setEditeIdea(idea)}
+                buttonText={<FaRegEdit className="w-5 h-5" />}
+                status={idea.status}
+              />
+            </div>
+          ))}
         </div>
-{editIdea &&(
-<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-<div className="bg-white p-4 rounded shadow-lg relative">
-<button 
-className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
-onClick={()=>setEditeIdea(null)}
->
+        {editIdea && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white p-4 rounded shadow-lg relative">
+              <button
+                className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
+                onClick={() => setEditeIdea(null)}
+              >
+                &times;
+              </button>
+              <div className="flex flex-wrap items-center ">
+                <div className="mt-4">
+                  <h4
+                    className={`${styles.heading4} bg-primary text-white p-2`}
+                  >
+                    Edit Idea
+                  </h4>
+                  <label>Title</label>
 
-&times;
-</button>
-<div className="flex flex-wrap items-center ">
-<div className="mt-4">
-<h4 className={`${styles.heading4} bg-primary text-white p-2`}>Edit Idea</h4>
-<label>Title</label>
-
- <input
- type="text"
- placeholder="عنواف الفكره "
- value={editIdea.title}
- onChange={(e)=>{setEditeIdea({...editIdea,title:e.target.value})}}
- className={`${styles.input}`}
- />
-  <br/>
-  <label>description</label>
- <textarea
- placeholder=",وصف الفكره"
- value={editIdea.description}
- onChange={(e)=>setEditeIdea({...editIdea,description:e.target.value})}
- className={`${styles.input}`}
-
- />
- <button onClick={updateIdea} className={`${styles.paragraph4} bg-secondary p-2 rounded-full mt-2 w-full`}>update Idea</button>
-</div>
-<div className="w-96 h-96">
- <img
-src={ideaedit}
-className="w-full h-full flip- "
-/>
-</div>
-</div>
-</div>
-</div>
-)}
-    </div>
+                  <input
+                    type="text"
+                    placeholder="عنواف الفكره "
+                    value={editIdea.title}
+                    onChange={(e) => {
+                      setEditeIdea({ ...editIdea, title: e.target.value });
+                    }}
+                    className={`${styles.input}`}
+                  />
+                  <br />
+                  <label>description</label>
+                  <textarea
+                    placeholder=",وصف الفكره"
+                    value={editIdea.description}
+                    onChange={(e) =>
+                      setEditeIdea({ ...editIdea, description: e.target.value })
+                    }
+                    className={`${styles.input}`}
+                  />
+                  <button
+                    onClick={updateIdea}
+                    className={`${styles.paragraph4} bg-secondary p-2 rounded-full mt-2 w-full`}
+                  >
+                    update Idea
+                  </button>
+                </div>
+                <div className="w-96 h-96">
+                  <img src={ideaedit} className="w-full h-full flip- " />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </main>
   );
 }
